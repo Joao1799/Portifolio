a3ca858a44d29b1fb1f245715fe983f1d183784c
joao1799 <joaovictoralves552@gmail.com>
2025-02-07 22:37:25 -0300

    ADD routing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './main/body/about/about.component';
+import { ContactComponent } from './main/body/contact/contact.component';
+import { SkillsComponent } from './main/body/skills/skills.component';
+import { WorkComponent } from './main/body/work/work.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' }, 
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'work', component: WorkComponent },
+];
+
--- src/app/main/body/body.component.ts
+++ src/app/main/body/body.component.ts
@@ -4,10 +4,11 @@ import { ContactComponent } from "./contact/contact.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { WorkComponent } from "./work/work.component";
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-body',
-  imports: [AboutComponent, ContactComponent, SkillsComponent, WorkComponent,CommonModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss',
   standalone: true
@@ -18,37 +19,4 @@ export class BodyComponent {
   contact:  boolean = false;
   skills:  boolean = false;
   work: boolean = false;
-
-
-  
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['menu']) {
-      this.abaSelecionada();
-    }
-  }
-
-  abaSelecionada() {
-    this.about = this.contact = this.skills = this.work = false;
-    
-    switch(this.menu){
-      case "About":
-        this.about = true;
-        break
-      case "Contact":
-        this.contact = true;
-        break
-      case "Skills":
-        this.skills = true;
-        break
-      case "Work":
-        this.work = true;
-        break
-    }
-    console.log("Estado atualizado:", {
-      about: this.about,
-      contact: this.contact,
-      skills: this.skills,
-      work: this.work
-    });
-  }
 }
