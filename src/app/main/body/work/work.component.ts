8587e2c9ac73ddc438bdcbbcb0daffad189aaf18
joao1799 <joaovictoralves552@gmail.com>
2025-02-10 21:25:27 -0300

    FIX css responsive and ADD WORK component

--- src/app/main/body/work/work.component.ts
+++ src/app/main/body/work/work.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 @Component({
   selector: 'app-work',
-  imports: [],
+  imports: [CardModule,ButtonModule,ScrollPanelModule],
   templateUrl: './work.component.html',
   styleUrl: './work.component.scss'
 })
